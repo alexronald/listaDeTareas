3c54a741b266b412f8c7e741ffd1e8c13ff2dc52
alex <alexronald43@gmail.com>
2023-03-26 11:28:42 -0500

    clase 03 completada

--- scrpt.js
+++ scrpt.js
@@ -1,8 +1,21 @@
 const btnAgregar = document.querySelector('[data-form-btn]');
 const crearCheck = (evento)=>{
     evento.preventDefault();
-    const inputAgregar = document.querySelector('[data-form-input]')
-    console.log(inputAgregar.value);
+    const inputAgregar = document.querySelector('[data-form-input]');
+    const list = document.querySelector('[data-list]')
+    const task = document.createElement('li');
+    task.classList.add("card")
+    const value = inputAgregar.value
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+    inputAgregar.value = "";
+    task.innerHTML = content
+    list.appendChild(task)
+    console.log(content);
+
 }
 
 btnAgregar.addEventListener('click', crearCheck);
